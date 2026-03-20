5f86f21a62fc70938bef23324f2ebb603615bb21
GabrielModog <modog.dev@gmail.com>
2026-03-20 08:16:18 -0300

    feat: adiciona rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 import './index.css'
-import App from './App.tsx'
+
+import AuthPage from './features/auth/pages/auth-page.tsx'
+import PostsPage from './features/posts/pages/posts-page.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/posts" element={<PostsPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
