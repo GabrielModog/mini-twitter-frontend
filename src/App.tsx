6080004a28a90cb3f86a6924e88e8269cc75cd97
GabrielModog <modog.dev@gmail.com>
2026-03-22 14:27:10 -0300

    refactor: atualiza raíz da aplicação com os providers necessários

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 
 import AuthPage from './features/auth/pages/auth-page'
 import PostsPage from './features/posts/pages/posts-page'
+import { ToastProvider } from './contexts/toast-context'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/query-client'
+import ProtectedRoute from './components/protected-router'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/posts" element={<PostsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AuthPage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/posts" element={<PostsPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ToastProvider>
   )
 }
 
